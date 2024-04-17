72621f22768f19cc533e533b263f6636e73e8790
Pankaj Kumar Maurya <153347011+pankajm1995@users.noreply.github.com>
2024-04-17 23:31:14 +0000

    update

---
+++ myapp/src/component/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <div>
+            <div className="footer">
+                <div className="div1">
+                    <p >For more details please contact on Call</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer
